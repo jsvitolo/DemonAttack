fca9544873310ff9d9e1a469e08e1e33156a2e9a
Jones Vitolo <jsvitolo@gmail.com>
2017-05-13 17:57:54 -0300

    add levels

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    
 	public GameObject[] hazards;
 	public Vector3 spawnValues;
 	public int hazardCount;
@@ -14,11 +16,15 @@ public class GameController : MonoBehaviour
 	public GUIText scoreText;
 	public GUIText restartText;
 	public GUIText gameOverText;
-    public GUIText levelText;
 
-	private bool gameOver;
+    private Text levelText;
+    private GameObject levelImage;
+    private bool doingSetup;
+
+    private bool gameOver;
 	private bool restart;
 	private int score;
+    private int level;
 
     public float levelDelay = 2f;
 
@@ -29,11 +35,24 @@ public class GameController : MonoBehaviour
 		restart  = false;
 		restartText.text  = "";
 		gameOverText.text = "";
-        levelText.text = ""; 
 		score = 0;
+        level = 1;
 		UpdateScore ();
 		StartCoroutine (SpawnWaves ());
+
+        //doingSetup = true; // ve se vai remover
+        levelImage = GameObject.Find("Level Image");
+        levelText = GameObject.Find("Level Text").GetComponent<Text>();
+        levelText.text = "NIVEL " + level;
+        //levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelDelay);
 	}
+
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        //doingSetup = false; // ver se vai remover
+    }
 
 	void Update ()
 	{
@@ -81,29 +100,35 @@ public class GameController : MonoBehaviour
 
 	void UpdateScore ()
 	{
-		scoreText.text = "Score:  " + score;
-	}
-
-	public void GameOver ()
-	{
-		gameOverText.text = "Game Over";
-		gameOver = true;
+        scoreText.text = "" + score;
 	}
 
     public void UpdateLevel ()
     {
-        if (score == 40)
+        switch (score)
         {
-            levelText.text = "Nivel 2";
-            hazardCount = 4;
-            waveWait = 4;
+            case 50 :
+                hazardCount = 4;
+                level = 2;
+                levelText.text = "NIVEL " + level;
+				levelImage.SetActive(true);
+				Invoke("HideLevelImage", levelDelay);
+                break;
+
+            case 150 :
+                hazardCount = 5;
+                level = 3;
+                spawnWait = 0.4f;
+				levelText.text = "NIVEL " + level;
+				levelImage.SetActive(true);
+				Invoke("HideLevelImage", levelDelay);
+				break;
         }
-        else if (score == 140)
-        {
-            levelText.text = "Nivel 3";
-            hazardCount = 6;
-            waveWait = 6;
-        }
-            
     }
+
+	public void GameOver ()
+	{
+		gameOverText.text = "GAME OVER";
+		gameOver = true;
+	}
 }
