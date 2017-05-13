dbe4366707ed41e7f88df75bacfd0cd6da66d245
Jones Vitolo <jsvitolo@gmail.com>
2017-05-13 09:31:54 -0300

    add assets and gitignore

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject[] hazards;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+    public GUIText levelText;
+
+	private bool gameOver;
+	private bool restart;
+	private int score;
+
+    public float levelDelay = 2f;
+
+
+	void Start ()
+	{
+		gameOver = false;
+		restart  = false;
+		restartText.text  = "";
+		gameOverText.text = "";
+        levelText.text = ""; 
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update ()
+	{
+        UpdateLevel();
+		if (restart)
+		{
+			if (Input.GetKeyDown (KeyCode.Space))
+			{
+				SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+//				Application.LoadLevel (Application.loadedLevel); // depreciado
+			}
+		}
+	}
+
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+				GameObject hazard = hazards [Random.Range (0, hazards.Length)];
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+
+			if (gameOver)
+			{
+				restartText.text = "Press 'Space' for Restart";
+				restart = true;
+				break;
+			}
+		}
+	}
+
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore ()
+	{
+		scoreText.text = "Score:  " + score;
+	}
+
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over";
+		gameOver = true;
+	}
+
+    public void UpdateLevel ()
+    {
+        if (score == 40)
+        {
+            levelText.text = "Nivel 2";
+            hazardCount = 4;
+            waveWait = 4;
+        }
+        else if (score == 140)
+        {
+            levelText.text = "Nivel 3";
+            hazardCount = 6;
+            waveWait = 6;
+        }
+            
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody rb;
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+	private AudioSource audioSource;
+	private Quaternion calibrationQuaternion;
+
+
+	void Start ()
+	{
+		CalibrateAccelerometer ();
+	}
+
+	void Update ()
+	{
+		audioSource = GetComponent<AudioSource >();
+
+		if (Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+				
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+			audioSource.Play ();
+		}
+	}
+
+	void CalibrateAccelerometer ()
+	{
+		Vector3 accelerationSnapshot = Input.acceleration;
+		Quaternion rotateQuaternion = Quaternion.FromToRotation (new Vector3 (0.0f, 0.0f, -1.0f), accelerationSnapshot);
+//		Quaternion rotateQuaternion = Quaternion.FromToRotation (new Vector3 (0.0f, 0.0f, -1.0f));
+		calibrationQuaternion = Quaternion.Inverse (rotateQuaternion);
+			
+	}
+
+	Vector3 FixAcceleration (Vector3 acceleration)
+	{
+		Vector3 fixedAcceleration = calibrationQuaternion * acceleration;
+		return fixedAcceleration;
+	}
+		
+	void FixedUpdate ()
+	{
+//		float moveHorizontal = Input.GetAxis ("Horizontal");
+//		float moveVertical   = Input.GetAxis ("Vertical");
+
+//		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		Vector3 accelerationRaw = Input.acceleration;
+		Vector3 acceleration = FixAcceleration (accelerationRaw);
+		Vector3 movement = new Vector3 (acceleration.x, 0.0f, acceleration.y);
+			
+		rb = GetComponent<Rigidbody>();
+
+		rb.velocity = movement * speed;
+		rb.position = new Vector3
+		(
+			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+				Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+
+	}
+}
